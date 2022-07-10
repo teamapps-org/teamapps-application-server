2ab22bfa03ed4ef2c9631180a24d454942e8069a
Matthias Bernstein <matthias.bernstein@teamapps.org>
2022-07-10 12:35:18 +0200

    updated header files

--- src/main/model/Protocol.java
+++ src/main/model/Protocol.java
@@ -1,8 +1,8 @@
 /*-
  * ========================LICENSE_START=================================
- * TeamApps Cluster
+ * TeamApps Application Server
  * ---
- * Copyright (C) 2021 - 2022 TeamApps.org
+ * Copyright (C) 2020 - 2022 TeamApps.org
  * ---
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
